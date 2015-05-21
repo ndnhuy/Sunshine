40090934adcc93b39729dc3792347ce97a658999
ndnhuy <ndnhuy2504@gmail.com>
2015-05-22 00:42:46 +0700

    SunShine

--- app/src/main/java/example/sunshine/MainActivity.java
+++ app/src/main/java/example/sunshine/MainActivity.java
@@ -1,14 +1,9 @@
 package example.sunshine;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -44,16 +39,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public static class PlaceholderFragment extends Fragment {
-        public PlaceholderFragment() {
-
-        }
-
-        @Nullable
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 }
---
+++ app/src/main/java/example/sunshine/WeatherDataHandler.java
@@ -0,0 +1,8 @@
+package example.sunshine;
+
+/**
+ * Created by PCPRO on 22/05/2015.
+ */
+public class WeatherDataHandler {
+
+}
